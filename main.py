8b4332c88ed4514dcbd91053c02299b4539e0aba
Berkay KARABAS <berkay.karabas091@gmail.com>
2023-09-02 17:07:03 +0300

    state game revision

--- main.py
+++ main.py
@@ -14,22 +14,23 @@ data = pd.read_csv("50_states.csv")
 state_list = data["state"].tolist()
 x_cor_list = data["x"].tolist()
 y_cor_list = data["y"].tolist()
+guess_state = []
 
-is_on = True
-while is_on:
-
-    answer_state = screen.textinput(title=f"Guess the States{score}/50", prompt="What's another State")
-
+while len(guess_state)<50:
+    answer_state = screen.textinput(title=f"Guess the States{len(guess_state)}/50", prompt="What's another State").title()
+    if answer_state == "Exit":
+        miss_state = []
+        for state in state_list:
+            if state not in guess_state:
+                miss_state.append(state)
+        new_data = pd.DataFrame(miss_state)
+        new_data.to_csv("learn_data.csv")
+        break
     if answer_state in state_list:
+        guess_state.append(answer_state)
         location=state_list.index(answer_state)
         x_cor=x_cor_list[location]
         y_cor=y_cor_list[location]
         bekos.goto(x_cor, y_cor)
         bekos.write(answer_state, move=False, align="center", font=FONT)
-        score += 1
-    else:
-        bekos.goto(0,0)
-        bekos.write(f"Game Over! there is not State in U.S. Your score is:{score}",move=False,align="center",font=FONT)
-        is_on = False
 
-turtle.mainloop()
